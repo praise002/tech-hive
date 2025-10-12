d7823f128bc2818f38b39534599f8ac018985961
Praise Idowu <ifeoluwapraise02@gmail.com>
2025-10-12 23:46:26 +0100

    fix: Correct import path for User model in create_roles management command

--- backend/apps/accounts/management/commands/create_roles.py
+++ backend/apps/accounts/management/commands/create_roles.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 
-from backend.apps.accounts.models import User
+from apps.accounts.models import User
 
 
 class Command(BaseCommand):
